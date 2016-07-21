ae87d9758403fe69c9b0ae98234a91a2f7bc7a5b
Chris Harris <c.harris@hotmail.com>
2016-07-21 14:12:12 +0200

    Moved the Lock class to a separate JavaScript file and made it publicly available by adding it to the cardmaker namespace.

---
+++ assets/js/common/cardmaker.lock-1.0.0.js
@@ -0,0 +1,85 @@
+(function(window, document, cardmaker, undefined) {
+    "use strict";
+    
+    /**
+     * The Lock is an access control mechanism for resources or operations.
+     *
+     * @author Chris Harris <c.harris@hotmail.com>
+     * @version 1.0.0
+     * @since 1.0.0
+     */
+    function Lock(locked) {
+        /**
+         * A reference to this object.
+         *
+         * @typedef {Lock}
+         * @private
+         */
+        var self = this;
+        
+        /**
+         * The lock state.
+         *
+         * @type {Boolean}
+         * @public
+         */
+        self.locked = false;
+        
+        /**
+         * Initialize the Lock.
+         *
+         * @param {Boolean} locked the initial lock state.
+         * @private
+         */
+        function init(locked) {
+            self.locked = (typeof locked === 'boolean') ? locked : false;
+        }
+        init(locked);
+    }
+    
+    /**
+     * Acquires the lock.
+     *
+     * @public
+     */
+    Lock.prototype.lock = function() {
+        this.locked = true;
+    }
+    
+    /**
+     * Acquire the lock only if is free at the time of invocation.
+     *
+     * @return {Boolean} true if the lock was acquired, otherwise false.
+     */
+    Lock.prototype.tryLock = function() {
+        var acquirable = (this.isLocked() === false);
+        if (acquirable) {
+            this.lock();
+        }
+        
+        return acquirable;
+    }
+        
+    /**
+     * Release the lock.
+     *
+     * @public
+     */
+    Lock.prototype.unlock = function() {
+        this.locked = false;
+    }
+    
+    /**
+     * Returns true if the lock is active.
+     *
+     * @return {Boolean} true if the lock is active, otherwise false.
+     * @public
+     */
+    Lock.prototype.isLocked = function() {
+        return (this.locked === true);
+    }
+    
+    // add Lock to namespace.
+    cardmaker.Lock = Lock;
+
+})(this, this.document, this.cardmaker = this.cardmaker || {});
--- assets/js/drawing/cardmaker.drawable-1.0.0.js
+++ assets/js/drawing/cardmaker.drawable-1.0.0.js
@@ -46,84 +46,6 @@
     InvalidateEvent.prototype.constructor = InvalidateEvent;
     
     /**
-     * The Lock is a control mechanism that can be used to allow or deny access to resources or operations.
-     *
-     * @author Chris Harris <c.harris@hotmail.com>
-     * @version 1.0.0
-     * @since 1.0.0
-     */
-    function Lock(locked) {
-        /**
-         * A reference to this object.
-         *
-         * @typedef {Lock}
-         * @private
-         */
-        var self = this;
-        
-        /**
-         * The lock state.
-         *
-         * @type {Boolean}
-         * @public
-         */
-        self.locked = false;
-        
-        /**
-         * Initialize the Lock.
-         *
-         * @param {Boolean} locked the initial lock state.
-         * @private
-         */
-        function init(locked) {
-            self.locked = (typeof locked === 'boolean') ? locked : false;
-        }
-        init(locked);
-    }
-    
-    /**
-     * Acquires the lock.
-     *
-     * @public
-     */
-    Lock.prototype.lock = function() {
-        this.locked = true;
-    }
-    
-    /**
-     * Acquire the lock only if is free at the time of invocation.
-     *
-     * @return {Boolean} true if the lock was acquired, otherwise false.
-     */
-    Lock.prototype.tryLock = function() {
-        var acquirable = (this.isLocked() === false);
-        if (acquirable) {
-            this.lock();
-        }
-        
-        return acquirable;
-    }
-        
-    /**
-     * Release the lock.
-     *
-     * @public
-     */
-    Lock.prototype.unlock = function() {
-        this.locked = false;
-    }
-    
-    /**
-     * Returns true if the lock is active.
-     *
-     * @return {Boolean} true if the lock is active, otherwise false.
-     * @public
-     */
-    Lock.prototype.isLocked = function() {
-        return (this.locked === true);
-    }
-    
-    /**
      * The Drawable class represents "something that can be drawn" onto a {@link cardmaker.Canvas} object.
      *
      * @author Chris Harris <c.harris@hotmail.com>
@@ -166,10 +88,10 @@
         /**
          * A lock that prevents the drawable from being invalidated while drawing.
          *
-         * @typedef {Lock}
+         * @typedef {cardmaker.Lock}
          * @private
          */
-        self.lock = new Lock();
+        self.lock = new cardmaker.Lock();
         
         /**
          * Initialize the Drawable.
@@ -179,7 +101,7 @@
         function init() {
             // call parent constructor.
             cardmaker.MVCObject.call(self);
-            
+
             self.matrix = new cardmaker.Matrix();
             self.matrix.addChangeListener(self.onPropertyChange.bind(self));
         }
@@ -407,7 +329,7 @@
      *
      * @param {cardmaker.Event} event - an Event object containing details about this event.
      * @see {@link cardmaker.Drawable#invalidate()}
-     * @see {@link Lock}
+     * @see {@link cardmaker.Lock}
      * @public
      */
     Drawable.prototype.onPropertyChange = function(event) {
